0a466c30c27eb044023107bc4b89ec7c9fadaea2
ralarconb <ralarconbazan@gmail.com>
2022-03-31 12:39:49 -0500

    .

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # cuy-backoffice
 - cuy bank backoffice system
-- clean
+- clean front end boilerplate
 ```powershell
 del src\logo.svg
 del src\App.css
 del src\index.css
 ```
-- add libraries
+- add front end libraries
 ```powershell
 npm install react-router-dom
+```
+- setup backend
+```powershell
+npm init -y
+npm install axios cors dotenv express mongoose
 ```
--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,64 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+// let people = [];
+
+// function exerciseList() {
+//   return people.map((person) => {
+//     console.log(person.name);
+//     return (
+//       <>currentexercise.name</>
+//       // <Exercise
+//       //   exercise={currentexercise}
+//       //   deleteExercise={this.deleteExercise}
+//       //   key={currentexercise._id}
+//       // />
+//     );
+//   });
+// }
+
+function getPeople() {
+  axios
+    .get("http://localhost:5000/people/")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 export default function People() {
+  const [value, setValue] = useState([]);
+  let people = [];
+  axios
+    .get("http://localhost:5000/people/")
+    .then((response) => {
+      // console.log(response.data);
+      // people = response.data;
+      // setValue(response.data);
+      console.log(people);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(people);
   return (
     <div>
-      <p>People</p>
+      <h3>Logged People</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>Username</th>
+            <th>Description</th>
+            <th>Duration</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        {/* <tbody>{exerciseList()}</tbody> */}
+      </table>
       <Link to="/">Back home</Link>
     </div>
   );
