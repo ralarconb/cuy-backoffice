947fc57213cfbe6a6c182a2f9762961f5ea2f8e7
ralarconb <ralarconbazan@gmail.com>
2022-04-10 14:32:20 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import People from "./components/People";
-import Person from "./components/Person";
-import Error from "./components/Error";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/people" element={<People />} />
-        <Route path="/people/:id" element={<Person />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
